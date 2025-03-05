c683a8be93f090d74334b3337f27811899763581
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2025-03-05 18:51:54 +0100

    Added test for hit function

--- src/test/java/e2/LogicTest.java
+++ src/test/java/e2/LogicTest.java
@@ -1,10 +1,10 @@
 package e2;
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LogicTest {
-  public static final int SIZE=3;
+  public static final int SIZE=4;
   public static final Pair<Integer, Integer> pawnPosition= new Pair<>(0,0);
   public static final Pair<Integer, Integer> knightPosition= new Pair<>(SIZE-1,SIZE-1);
   public Logics logics;
@@ -23,6 +23,16 @@ public class LogicTest {
   @Test
   public void testHasPawn() {
     assertTrue(logics.hasPawn(pawnPosition.getX(), pawnPosition.getY()));
+  }
+
+  @Test
+  public void testHit(){
+    assertFalse(logics.hit(0,0));
+  }
+
+  @Test
+  public void testHitOutOfBounds(){
+    assertThrows(IndexOutOfBoundsException.class, ()->logics.hit(-1,-1));
   }
 
 }
